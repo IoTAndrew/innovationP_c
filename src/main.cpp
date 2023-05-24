1e7214c8e9a89d7f08b07d9d6c5a0743666e6bed
Andy <al3286957@gmail.com>
2023-05-24 20:59:08 +0300

    ready to work with grove sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <Controllino.h>
 #include <SoftwareSerial.h>
+#include <Ultrasonic.h>
 
 String sensorstring;
 
 #define mySerial Serial1
-const byte water_level_trig_pin = 5;     // Digital 3   X1
-const byte water_level_echo_pin = 6;     // Digital 4   X1
+//const byte water_level_trig_pin = 5;     // Digital 3   X1
+//const byte water_level_echo_pin = 6;     // Digital 4   X1
+
+Ultrasonic ultrasonic(6);
 
 String fromPi;
 
@@ -17,11 +20,12 @@ void waterLevel();
 
 void setup() {
 
-  pinMode(water_level_trig_pin, OUTPUT);
-  pinMode(water_level_echo_pin, INPUT);
+  //pinMode(water_level_trig_pin, OUTPUT);
+  //pinMode(water_level_echo_pin, INPUT);
+
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  mySerial.begin(9600);
+  Serial.begin(19200);
+  mySerial.begin(19200);
   sensorstring.reserve(50);
   
   while (!Serial) {
@@ -56,7 +60,6 @@ void loop() {
     String response = temp1 + "&" + temp2 + "&" + ph + "&" + hum + "&" + ec + "&" + tds + "&" + orp + "&" + co2;
     //mySerial.println(response);
     waterLevel(); 
-    delay(1000);
   }
   //mySerial.print(sensorstring);
 }
@@ -65,6 +68,7 @@ void waterLevel(void){
     // 200 ms
     float water_level_sum = 0; // sum declaration
     for (int i=0 ; i<5 ; i++){ // 5 samples are taken
+    /*
       digitalWrite(water_level_trig_pin, LOW); // Clears the water_level_trig_pin condition first
       delayMicroseconds(2);
       digitalWrite(water_level_trig_pin, HIGH); // Sets the water_level_trig_pin HIGH (ACTIVE) for 10 microseconds (time for 8 cycle sonic bursts)
@@ -73,11 +77,14 @@ void waterLevel(void){
       duration = pulseIn(water_level_echo_pin, HIGH); // Reads the water_level_echo_pin, returns the sound wave travel time in microseconds
       distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
       water_level_sum = water_level_sum + distance; // Sum calculation
+      */
+    distance = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+    water_level_sum = water_level_sum + distance;
     }
     av_dist = round(water_level_sum / 5.0); // one average value of distance in cm
     water_level = map(av_dist, 2, 22, 100, 0); // one average value of distance in % | sensor's range starts from 2 cm (fixed)
-    mySerial.print("  Distance in cm: "); // prints average of 5 samples in cm
-    mySerial.print(av_dist);
-    mySerial.print("\t Distance in %: "); // prints average of 5 samples in %
-    mySerial.println(water_level); 
+    //mySerial.print("  Distance in cm: "); // prints average of 5 samples in cm
+    mySerial.println(av_dist);
+    //mySerial.print("\t Distance in %: "); // prints average of 5 samples in %
+    //mySerial.println(water_level); 
 }
